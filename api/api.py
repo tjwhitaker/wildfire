004d8660ac2238e5337a32a5d1cc63a50cb740df
Tim Whitaker <camp4climber@gmail.com>
2017-09-10 14:02:44 -0600

    Stub out predict model and add test json

--- api/api.py
+++ api/api.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from sklearn.externals import joblib
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+# Expecting json to be an object with these fields:
+#
+# {
+#   "model": "lr", "ransac", "rfr", "sgd", or "svr"
+#   "observations": {
+#     "x": Int from 0-9,
+#     "y": Int from 0-9,
+#     "month": "jan", "feb", "mar", "apr", "may", "jun", 
+#              "jul", "aug", "sep", "oct", "nov", or "dec"
+#     "day": "mon", "tue", "wed", "thu", "fri", "sat", or "sun"
+#     "ffmc": Float,
+#     "dmc": Float,
+#     "dc": Float,
+#     "isi": Float,
+#     "temp": Float,
+#     "rh": Float,
+#     "wind": Float,
+#     "rain": Float
+#   }
+# }
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    model = data['model']
+    observations = list(data['observations'].values())
+
+    print(model, observations)
+    return
 
 if __name__ == '__main__':
     app.run(debug=True)
